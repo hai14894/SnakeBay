c30ce046f102b1b4b62cd5fb596dca73156036d6
hai14894 <hohoanghai14894@gmail.com>
2020-04-27 19:16:23 -0700

    added User authentication

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,7 @@
 class ListingsController < ApplicationController
-    before_action :set_listing, only: [:show, :edit, :update, :destroy]
-
+    before_action :authenticate_user!
+    before_action :set_listing, only: [:show]
+    before_action :set_user_listing, only: [:edit, :update, :destroy]
     def index
         @listings = Listing.all
     end
@@ -14,8 +15,8 @@ class ListingsController < ApplicationController
     end
 
     def create
-        #finish logic for creating a record
-        @listing = Listing.create(listing_params)
+
+        @listing = current_user.listings.create(listing_params)
         if @listing.errors.any?
             set_breeds_and_sexes
             render "new"
@@ -48,6 +49,15 @@ class ListingsController < ApplicationController
 
     def set_listing
         @listing = Listing.find(params[:id])
+    end
+
+    def set_user_listing
+        id = params[:id]
+        @listing = current_user.listings.find_by_id(id)
+    
+        if @listing == nil
+            redirect_to listings_path
+        end
     end
 
     def set_breeds_and_sexes
